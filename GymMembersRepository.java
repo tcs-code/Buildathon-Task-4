13da80f3b8546c01d326f678e5018c2eb3afcae6
Rahul V <107417337+tcs-code@users.noreply.github.com>
2025-02-26 14:16:26 +0530

    Delete GymMembersRepository.java

--- GymMembersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gymmembership.gymmembership.Repository;
-
-import com.gymmembership.gymmembership.Models.GymMembers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GymMembersRepository extends JpaRepository<GymMembers, Long> {
-}
